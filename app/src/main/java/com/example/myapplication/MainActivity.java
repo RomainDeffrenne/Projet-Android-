04056682480a4a46f99b48ebaca8ad3c656e0bae
valentin <valentin.chivet@isen.yncrea.fr>
2020-02-27 15:00:07 +0100

    test 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //ceci est un test
+        setContentView(R.layout.activity_main); //ceci est un test 2
     }
 }
